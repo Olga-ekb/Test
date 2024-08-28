192d449ab987b45cddc28f6884ea9a2dd1ae31ab
Olga-ekb <volgaekb@mail.ru>
2024-08-28 12:12:09 +0500

    first commit

---
+++ add.js
@@ -0,0 +1,9 @@
+const firstName = 'Olga'
+const Age = '38'
+const City = 'Ekaterinburg'
+const Hobby = 'Sport'
+
+console.log ('Hello! My name is', firstName)
+console.log ('I am', Age, 'years old')
+console.log ('I live in', City)
+console.log ('I like', Hobby)
