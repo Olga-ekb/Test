e51305663ed3d96d230ac36e44d1d69200335d8a
Olga-ekb <volgaekb@mail.ru>
2024-08-29 15:32:44 +0500

    first commit

---
+++ lesson_2.2/add.js
@@ -0,0 +1,6 @@
+let titleProject = 'Оценка стоимости';
+let screensValue = 'Шаблонные, с уникальным дизайном, с анимациями';
+let screenPrice = 300;
+let percentage = 30;
+let fullPrice = 3500;
+let responsive = true;
